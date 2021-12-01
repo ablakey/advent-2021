87e17406639765743f56e700d680cf24271a1d2d
ablakey <ablakey@locusrobotics.com>
2021-12-01 16:43:14 -0500

    update

--- src/day1.ts
+++ src/day1.ts
@@ -1,1 +1,13 @@
-console.log("Hello");
+import { promises as fs } from "fs";
+
+async function getInput() {
+  const input = await fs.readFile("./day1_input.txt", "utf-8");
+  const parsedInput = input
+    .split("\n")
+    .map((i) => Number.parseInt(i))
+    .filter((i) => !isNaN(i));
+
+  return parsedInput;
+}
+
+const x = getInput();
